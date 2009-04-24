30502828946e0659bf2bbb40643a696f988c26d3
Francis <Francis@LEOPC1>
2009-04-24 14:22:24 +0800

    complete CSOrderMaker

--- src/quiz/meal/csp/CSOrderMaker.java
+++ src/quiz/meal/csp/CSOrderMaker.java
@@ -2,15 +2,24 @@ package quiz.meal.csp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import jopt.csp.CspSolver;
+import jopt.csp.search.SearchAction;
+import jopt.csp.search.SearchActions;
+import jopt.csp.search.SearchGoal;
+import jopt.csp.search.SearchGoals;
+import jopt.csp.search.SearchTechniques;
 import jopt.csp.variable.CspConstraint;
 import jopt.csp.variable.CspIntExpr;
 import jopt.csp.variable.CspIntVariable;
-import jopt.csp.variable.CspMath;
 import jopt.csp.variable.CspVariableFactory;
+import jopt.csp.variable.PropagationFailureException;
+
+import org.apache.log4j.Logger;
+
 import quiz.meal.Menu;
 import quiz.meal.OrderHelper;
 import quiz.meal.OrderMaker;
@@ -20,73 +29,124 @@ import quiz.meal.model.Item;
 import quiz.meal.model.Meal;
 
 public class CSOrderMaker implements OrderMaker {
-	private Menu menu;
-	
-	public CSOrderMaker() {
-		menu = new SimpleMenu();
-	}
-	
-	@Override
-	public List<Item> order(Item... wantedItems) {
+    private Logger log = Logger.getLogger(CSOrderMaker.class.getName());    
+    private Menu menu;
+    private Item[] allItems;
+
+    public CSOrderMaker() {
+        menu = new SimpleMenu();
+        allItems = menu.getAllItems().values().toArray(new Item[0]);
+    }
+
+    @Override
+    public List<Item> order(Item... wantedItems) {
         CspSolver solver = CspSolver.createSolver();
-        CspVariableFactory varFactory  = solver.getVarFactory();
-        CspMath varMath = varFactory.getMath();
-        
+        CspVariableFactory varFactory = solver.getVarFactory();
+        solver.setAutoPropagate(true);
+
         List<Food> wantedFood = OrderHelper.getItemAsFoodList(Arrays.asList(wantedItems));
         Map<Item, Integer> foodCount = OrderHelper.getItemCount(wantedFood);
-        Item[] items = menu.getAllItems().values().toArray(new Item[0]);
+        Item[] items = getRelatedItems(wantedItems);
         
         List<CspIntVariable> orderVar = new ArrayList<CspIntVariable>();
-        for(Item f : items) {
-        	CspIntVariable w = varFactory.intVar(f.getName(), 
-        			foodCount.get(f) == null ? 0 : foodCount.get(f), 
-					wantedFood.size());
-        	orderVar.add(w);
+        for (Item f : items) {
+            CspIntVariable w = varFactory.intVar(f.getName(), 0, wantedFood.size());
+            orderVar.add(w);
         }
-        
-        // wantedX <= foodX + mean coontains foodX
-        int[][] foodMealMatrix = new int[items.length][items.length];
-        for(int i=0; i<items.length; i++) {
-            Item item = items[i];
-            if (item instanceof Food) {
-                foodMealMatrix[i][i] = 1;
+        int maxPrice = ((int) maxPrice(wantedFood) * 10);
+        CspIntVariable priceVar = varFactory.intVar("price", 0, maxPrice);
 
-            } else if (item instanceof Meal) {
-                Meal meal = (Meal) item;
-                for(int j=0; j<items.length; j++) {
-                    List<Food> mealFood = meal.getFood();
-                    for(int k=0; k<mealFood.size(); k++) {
-                        if ( mealFood.get(k).equals((items[j])) ) {
-                            foodMealMatrix[j][i]++;
+        try {
+            CspIntExpr priceExpr = priceVar;
+            for (int i = 0; i < items.length; i++) {
+                Item item = items[i];
+                CspIntVariable oX = orderVar.get(i);
+                if (item instanceof Food) {
+                    CspIntExpr sum = oX;
+                    int itemCount = foodCount.containsKey(item) ? foodCount.get(item) : 0;
+                    
+                    for (int j = 0; j < items.length; j++) {
+                        if (items[j] instanceof Meal) {
+                            Meal m = (Meal) items[j];
+                            if (m.getFood().contains(item)) {
+                                sum = sum.add(orderVar.get(j));
+                            }
                         }
                     }
-
+                    
+                    // ordered_food(X) + SUM(ordered_meal_with_food(X)) >= wanted_food(x)
+                    CspConstraint foodConstraint = sum.geq(itemCount);
+                    solver.addConstraint(foodConstraint);
                 }
+            
+                // total price = sum ordered_food(X) * price(X)
+                priceExpr = priceExpr.add(oX.multiply((int)(item.getPrice()*10)));
+            }
+            log.debug("price = " + priceExpr.toString());
+            
+            SearchTechniques tech = solver.getSearchTechniques();
+            SearchGoals goals = solver.getSearchGoals();
+            SearchGoal minimizePriceGoal = goals.minimize(priceExpr);
+
+            SearchActions actions = solver.getSearchActions();
+            SearchAction action = actions.generate(orderVar.toArray(new CspIntVariable[0]));            
+            solver.solve(action, minimizePriceGoal, tech.dfs());
+            
+            log.info(" result order = " + orderVar);
+        } catch (PropagationFailureException e) {
+            log.error("error propagate constraint", e);
+        }
+        
+        return getOrderBySolution(items, orderVar);
+    }
+    
+    private double maxPrice(List<Food> wantedFood) {
+        double price = 0.0;
+        for(Food f : wantedFood) {
+            price += f.getPrice();
+        }
+        return price;
+    }
+    
+    private List<Item> getOrderBySolution(Item[] items, List<CspIntVariable> orderItemCount) {
+        List<String> orderItemNames = new ArrayList<String>();
+        for(int i=0; i<items.length; i++) {
+            // number of order item i
+            int val = orderItemCount.get(i).getMin();            
+            // add order
+            for(int j=0; j<val; j++) {
+                orderItemNames.add(orderItemCount.get(i).getName());
+            }
+        }
+        return menu.getItems(orderItemNames.toArray(new String[orderItemNames.size()]));
+    }
+    
+    /**
+     * Filter out items that are not related to the wanted food. "Related to" means the item is a wanted food, 
+     * or a meal containing wanted food. 
+     * 
+     * @param allItems all items
+     * @param wantedFood food wanted
+     * @return array of items that are only related to the wanted order
+     */
+    private Item[] getRelatedItems(Item[] wantedFood) {
+        ArrayList<Item> itemList = new ArrayList<Item>(Arrays.asList(allItems));
+        Iterator<Item> iter = itemList.iterator();
+        while(iter.hasNext()) {
+            Item item = iter.next();
+            boolean isWanted = false;
+            for(Item wanted : wantedFood) {
+                if (item.equals(wanted) || (item instanceof Meal && ((Meal)item).getFood().contains(wanted))) {
+                    // retain
+                    isWanted = true;
+                    break;
+                }
+            }
+            if (!isWanted) {
+                iter.remove();
             }
         }
         
-        for(int i=0; i<items.length; i++) {
-        	Item item = items[i];
-        	CspIntVariable oX = orderVar.get(i);
-        	CspIntExpr sum = oX;
-        	
-        	for(int j=0; j<items.length; j++) {
-        		if (items[j] instanceof Meal && ((Meal)items[j]).getFood().contains(items[i])) {
-        			sum = sum.add(orderVar.get(i));
-        		}
-        	}
-        	
-        	CspConstraint constraint = sum.geq(foodCount.containsKey(item) ?  foodCount.get(item) : 0);
-        	//solver.addConstraint(constraint);
-        }
-        
-//        for(int i=0; i<items.length; i++) {
-//            model.addConstraint(Choco.geq(scalar(foodMealMatrix[i], orderItemCountArr), 
-//                    wantedItemCountVar.get(i)));
-//        }
-
-        
-        return null;
-	}
-
+        return itemList.toArray(new Item[itemList.size()]);
+    }
 }
